b7734893d495e4ec2981882426d56b5473d313d3
Pat Hickey <pat@moreproductive.org>
2013-03-14 09:53:19 -0700

    quickcheck improved

--- Language/Sprout/Test/QuickCheck.hs
+++ Language/Sprout/Test/QuickCheck.hs
@@ -1,6 +1,7 @@
 
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Language.Sprout.Test.QuickCheck where
 
@@ -11,23 +12,72 @@ import Language.Sprout.Machine
 
 import Test.QuickCheck
 
-data Rep = forall t. Rep (E t)
+newtype Binop a = Binop (E a -> E a -> E a)
 
-instance Arbitrary Rep where
+instance Arbitrary (Binop Int32) where
+  arbitrary = elements [ Binop Add, Binop Sub, Binop Mul, Binop Div ]
+
+instance Arbitrary (Binop Word32) where
+  arbitrary = elements [ Binop Add, Binop Sub, Binop Mul, Binop Div ]
+
+instance Arbitrary (E Bool) where
   arbitrary = oneof
     [ do i <- arbitrary
-         return (Rep (ConstE (i :: Word32)))
-    , do i <- arbitrary
-         return (Rep (ConstE (i :: Int32)))
-    , do i <- arbitrary
-         return (Rep (ConstE (i :: Bool)))
-    , do
-        a <- arbitrary
-        b <- arbitrary
-        case a of
-          Rep c ->
-            case b of
-              Rep d ->
-                return (Rep (Add c d))
+         return (ConstE (i :: Bool))
+    , do a <- arbitrary
+         b <- arbitrary
+         return (And a b)
+    , do a <- arbitrary
+         return (Not a)
+    , do a <- arbitrary
+         b <- arbitrary
+         c <- arbitrary
+         return (Mux (a :: E Bool) (b :: E Bool) (c :: E Bool ))
+    , do a <- arbitrary
+         b <- arbitrary
+         return (Eq (a :: E Bool) (b :: E Bool ))
+    , do a <- arbitrary
+         b <- arbitrary
+         return (Eq (a :: E Word32) (b :: E Word32))
+    , do a <- arbitrary
+         b <- arbitrary
+         return (Eq (a :: E Int32) (b :: E Int32))
+    , do a <- arbitrary
+         b <- arbitrary
+         return (Lt (a :: E Word32) (b :: E Word32))
+    , do a <- arbitrary
+         b <- arbitrary
+         return (Lt (a :: E Int32) (b :: E Int32))
     ]
 
+instance Arbitrary (E Int32) where
+  arbitrary = oneof
+    [ do i <- arbitrary
+         return (ConstE (i :: Int32))
+    , do a <- arbitrary
+         b <- arbitrary
+         op <- arbitrary
+         case op of
+           Binop o ->
+             return (o (a :: E Int32) (b :: E Int32))
+    , do a <- arbitrary
+         b <- arbitrary
+         c <- arbitrary
+         return (Mux (a :: E Bool) (b :: E Int32) (c :: E Int32))
+    ]
+
+instance Arbitrary (E Word32) where
+  arbitrary = oneof
+    [ do i <- arbitrary
+         return (ConstE (i :: Word32))
+    , do a <- arbitrary
+         b <- arbitrary
+         op <- arbitrary
+         case op of
+           Binop o ->
+             return (o (a :: E Word32) (b :: E Word32))
+    , do a <- arbitrary
+         b <- arbitrary
+         c <- arbitrary
+         return (Mux (a :: E Bool) (b :: E Word32) (c :: E Word32))
+    ]
